3bf2cbd2d7c2ad4054d9ff69b886756b3effc8a0
Huynh Quoc Khanh aka Kan <70838508+Kan-Kzeit@users.noreply.github.com>
2022-04-28 18:51:30 +0700

    fixed work in android device

--- burn.lua
+++ burn.lua
@@ -14,68 +14,42 @@ kernel.category = "filter"
 kernel.name = "burn"
 kernel.isTimeDependent = true
 
-kernel.vertexData   = {
-  {
-    name = "duration",
+kernel.uniformData = {
+	{
+		name = "duration",
     default = 0.5, 
     min = 0,
-    max = 100,
-    index = 0, 
-  },
+    max = 100.0,
+		type= "float",
+		index = 0, -- u_UserData0
+	},
   {
-    name = "startTime",
+		name = "startTime",
     default = 0.0, 
-    index = 1,
-  }
+    min = 0.0,
+		type= "float",
+		index = 1, -- u_UserData1
+	}
 }
+
 kernel.fragment =
 [[
-  P_COLOR vec4 ash = vec4(0, 0, 0, 1);
-  P_COLOR vec4 fire = vec4(127/255, 180/255, 128/255, 0.5);
-  
-  P_COLOR int OCTAVES = 6;
+  uniform P_COLOR float u_UserData0; // duration
+  uniform P_COLOR float u_UserData1; // startTime
 
-  P_COLOR float rand(vec2 coord){
-    return fract(sin(dot(coord, vec2(12.9898, 78.233)))* 43758.5453123);
-  }
-  
-  P_COLOR float noise(vec2 coord){
-    P_COLOR vec2 i = floor(coord);
-    P_COLOR vec2 f = fract(coord);
-
-    P_COLOR float a = rand(i);
-    P_COLOR float b = rand(i + vec2(1.0, 0.0));
-    P_COLOR float c = rand(i + vec2(0.0, 1.0));
-    P_COLOR float d = rand(i + vec2(1.0, 1.0));
-  
-    P_COLOR vec2 cubic = f * f * (3.0 - 2.0 * f);
-  
-    return mix(a, b, cubic.x) + (c - a) * cubic.y * (1.0 - cubic.x) + (d - b) * cubic.x * cubic.y;
-  }
-  
-  P_COLOR float fbm(vec2 coord){
-    P_COLOR float value = 0.0;
-    P_COLOR float scale = 0.5;
-  
-    for(int i = 0; i < OCTAVES; i++){
-      value += noise(coord) * scale;
-      coord *= 2.0;
-      scale *= 0.5;
-    }
-    return value;
-  }
+  P_COLOR vec4 ash = vec4(0.0, 0.0, 0.0, 1.0);
+  P_COLOR vec4 fire = vec4(127/255, 180/255, 128/255, 1.0);
   
     P_COLOR vec4 FragmentKernel( P_UV vec2 texCoord )
     {
-        P_DEFAULT float totalTime = 0.0; 
-
         P_COLOR vec4 color = texture2D( CoronaSampler0, texCoord );
-        P_COLOR vec4 tex = texture2D( CoronaSampler0, texCoord);
+        P_COLOR vec4 tex = texture2D( CoronaSampler1, texCoord );
+        P_COLOR vec4 tex2 = texture2D( CoronaSampler1, texCoord );
+
+        P_COLOR float noise = (tex.r + tex.g + tex.b)/3.0;
+        P_COLOR float thickness = 0.05;
         
-        P_COLOR float noise = fbm(texCoord * 6.0);
-        P_COLOR float thickness = 0.1;
-        
-        P_COLOR float outer_edge = (CoronaTotalTime - CoronaVertexUserData.y)/CoronaVertexUserData.x;
+        P_COLOR float outer_edge = (CoronaTotalTime - u_UserData1)/u_UserData0;
         P_COLOR float inner_edge = outer_edge + thickness;
 
         if (noise < inner_edge) {
@@ -90,11 +64,11 @@ kernel.fragment =
         }
 
         if (noise < outer_edge) {
-          color.a = 1 - (outer_edge - noise)/ 0.03;
+          color.a = 1.0 - (outer_edge - noise)/ 0.03;
           color.a = clamp(color.a, 0.0, 1.0);
         }
         
-        color.a *= tex.a;
+        color.a *= tex2.a;
         
         return CoronaColorScale(color);
       }
--- main.lua
+++ main.lua
@@ -1,8 +1,21 @@
-local pirate = display.newImageRect("pirate.png", 109, 111)
+local pirate = display.newImageRect("pirate.png", 200, 200)
+
 pirate.x, pirate.y = display.contentCenterX, display.contentCenterY
 
+
 require "burn"
-pirate.fill.effect = "filter.custom.burn"
-pirate.fill.effect.startTime = system.getTimer()/1000
-pirate.fill.effect.duration = 2
+
+timer.performWithDelay(2000,function()
+
+    pirate.fill = {
+        type = "composite",
+        paint1 = { type="image", filename="pirate.png" },
+        paint2 = { type="image", filename="noise.jpg" }
+    }
+  
+    pirate.fill.effect = "filter.custom.burn"
+    pirate.fill.effect.startTime = system.getTimer()/1000
+    pirate.fill.effect.duration = 2
+    graphics.undefineEffect( "filter.custom.burn")
+end, -1)
 
